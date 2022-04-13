46ec8038d09b5b4ec56374a9c492d024acd93d8d
arthurariza <tucaariza@gmail.com>
2022-04-12 23:17:54 -0300

    feat: reading file in streams and casting the price

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -3,10 +3,18 @@ defmodule ReportsGenerator do
     file_path = "reports/#{filename}"
 
     file_path
-    |> File.read()
-    |> handle_file()
+    |> File.stream!()
+    |> Enum.map(fn line -> parse_line(line, 2) end)
+
+    # |> Enum.map(&parse_line(&1, 2))
   end
 
-  defp handle_file({:ok, file}), do: file
-  defp handle_file({:error, reason}), do: reason
+  defp parse_line(line, position) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(position, &String.to_integer/1)
+
+    # |> List.update_at(position, fn elem -> String.to_integer(elem) end)
+  end
 end
