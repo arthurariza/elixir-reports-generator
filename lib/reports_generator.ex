88e552268ab16277d14060580e279561e97f2a5b
arthurariza <tucaariza@gmail.com>
2022-04-12 22:48:46 -0300

    refactor: using pipe operator

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,8 +1,12 @@
 defmodule ReportsGenerator do
   def build(filename) do
-    case File.read("reports/#{filename}") do
-      {:ok, file} -> file
-      {:error, reason} -> reason
-    end
+    file_path = "reports/#{filename}"
+
+    file_path
+    |> File.read()
+    |> handle_file()
   end
+
+  defp handle_file({:ok, file}), do: file
+  defp handle_file({:error, reason}), do: reason
 end
